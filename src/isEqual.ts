3894a5a29d422ca907dc8f938fee9f6135b63797
Seraj Vahdati <svahdati71@gmail.com>
2021-12-13 17:30:43 +0330

    fix(isEqual): change param names

--- src/isEqual.ts
+++ src/isEqual.ts
@@ -1,14 +1,14 @@
 const stringy = (arr: any) => '' + arr.map(JSON.stringify).sort();
 
-const isEqual = (arr1: any, arr2: any) => {
-  if (typeof arr1 !== 'object') {
-    return arr1 === arr2;
+const isEqual = (first: any, second: any) => {
+  if (typeof first !== 'object') {
+    return first === second;
   }
-  if (typeof arr1 === 'object' && !Array.isArray(arr1)) {
-    return JSON.stringify(arr1) === JSON.stringify(arr2);
+  if (typeof first === 'object' && !Array.isArray(first)) {
+    return JSON.stringify(first) === JSON.stringify(second);
   }
-  if (Array.isArray(arr1)) {
-    return stringy(arr1) === stringy(arr2);
+  if (Array.isArray(first)) {
+    return stringy(first) === stringy(second);
   }
 };
 
