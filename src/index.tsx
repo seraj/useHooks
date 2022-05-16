d3f73fa2d48cd300cffbc43b617b5d9c66b588ff
Seraj Vahdati <svahdati71@gmail.com>
2022-05-16 11:27:56 +0430

    feat(useInterval): useInterval hook implemented

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import useDocVisible from './useDocVisible';
 import isEmpty from './isEmpty';
 import uniqueArray from './uniqueArray';
 import useMediaQuery from './useMediaQuery';
+import useInterval from './useInterval';
 import uniqueKey from './uniqueKey';
 import persianSort from './persianSort';
 import isEqual from './isEqual';
@@ -21,6 +22,7 @@ export {
   useWindowSize,
   useDocVisible,
   useMediaQuery,
+  useInterval,
   isEmpty,
   uniqueArray,
   uniqueKey,
---
+++ src/useInterval.ts
@@ -0,0 +1,30 @@
+import { useRef, useState } from 'react';
+
+function useInterval(fn: () => void, interval: number) {
+  const [active, setActive] = useState(false);
+  const intervalRef = useRef<number>();
+
+  const start = () => {
+    if (!active) {
+      setActive(true);
+      intervalRef.current = window.setInterval(fn, interval);
+    }
+  };
+
+  const stop = () => {
+    setActive(false);
+    window.clearInterval(intervalRef.current);
+  };
+
+  const toggle = () => {
+    if (active) {
+      stop();
+    } else {
+      start();
+    }
+  };
+
+  return { start, stop, toggle, active };
+}
+
+export default useInterval;
