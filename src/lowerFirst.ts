0e6a977f03319c5206f961326b6acd5843f80854
Seraj Vahdati <svahdati71@gmail.com>
2022-06-11 15:15:01 +0430

    fix: fix export names

--- src/lowerFirst.ts
+++ src/lowerFirst.ts
@@ -1,4 +1,4 @@
-export function lowerFirst(value: string) {
+function lowerFirst(value: string) {
   return typeof value !== 'string'
     ? ''
     : value.charAt(0).toLowerCase() + value.slice(1);
